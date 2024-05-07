bfdca04cd1eb76db93c8e0344dcd834308c30b6f
Sam Minot <sminot@fredhutch.org>
2024-05-07 13:53:56 -0700

    Outputs from convert are nested further

--- templates/merge.sh
+++ templates/merge.sh
@@ -2,7 +2,7 @@
 set -e
 
 samestr merge \
-    --input-files out_convert/*.npz \
+    --input-files out_convert/*/*.npz \
     --marker-dir samestr_db/ \
     --nprocs ${task.cpus} \
     --output-dir out_merge/
